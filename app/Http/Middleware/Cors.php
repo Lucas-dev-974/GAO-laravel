b7eea0c2f29d8860fc28a9aa2659a915baa2a08c
lcs lvn <lucas.lvn97439@gmail.com>
2022-08-12 09:54:52 +0200

    Update Cors headers configurations

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -5,8 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 
-class Cors
-{
+class Cors{
     /**
      * Handle an incoming request.
      *
@@ -18,7 +17,6 @@ class Cors
     {
         return $next($request)
         ->header('Access-Control-Allow-Origin', '*')
-        ->header('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, DELETE')
-        ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization');
+        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     }
 }
