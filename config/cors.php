bc3734811263cde30ac8340982044bd3ee332e61
lcs lvn <lucas.lvn97439@gmail.com>
2022-08-12 09:55:41 +0200

    Cors Update allow origin

--- config/cors.php
+++ config/cors.php
@@ -2,9 +2,9 @@
 
 return [
 
-    'paths' => ['api/*'],
+    'paths' => ['*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['http://localhost:8080'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => ['*'],
